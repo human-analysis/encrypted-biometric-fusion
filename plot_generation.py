feb7cf771f2e87cd36d28feeaef21dc34ad5902e
Luke Sperling <46566726+sperli14@users.noreply.github.com>
2021-11-22 19:23:12 -0500

    Toy dataset visualization added

--- plot_generation.py
+++ plot_generation.py
@@ -30,7 +30,56 @@ def plot_loss():
     if not os.path.exists("figures/loss"):
         os.mkdir("figures/loss")
     fig.write_image("figures/loss/train_loss.png")
-    #fig.show()
+
+
+def plot_dataset():
+    if not os.path.exists("figures"):
+        os.mkdir("figures")
+    if not os.path.exists("figures/original_dataset"):
+        os.mkdir("figures/original_dataset")
+        
+    a_file = open("data/A_values.txt",'r')
+    A = []
+    for line in a_file:
+        a = torch.tensor(ast.literal_eval(line.strip())).unsqueeze(dim=0)
+        A.append(a)
+    A_final = torch.Tensor(len(A),A[0].shape[0])
+    torch.cat(A, out=A_final,dim=0)
+    
+    b_file = open("data/B_values.txt",'r')
+    B = []
+    for line in b_file:
+        b = torch.tensor(ast.literal_eval(line.strip())).unsqueeze(dim=0)
+        B.append(b)
+    B_final = torch.Tensor(len(B),B[0].shape[0])
+    torch.cat(B, out=B_final,dim=0)
+        
+    l_file = open("data/L_values.txt",'r')
+    L = []
+    for line in l_file:
+        L.append(int(float(line.strip())))
+        
+        
+    
+    data_dictA = {"a1":A_final[:,0],"a2":A_final[:,1],"a3":A_final[:,2]," ":["class "+str(l) for l in L]}
+    dfA = pandas.DataFrame(data_dictA)
+
+    figA = px.scatter_3d(dfA,x="a1",y="a2",z="a3",color=" ")
+    
+    
+    num_class = len(set(L))
+    samples_per_class = L.count(L[0])
+    colors = list(set(L))
+    colors = [3*color for color in colors]
+    titles = ["class "+str(i) for i in range(num_class)]
+    
+    figB = go.Figure()
+    for i in range(num_class):
+        figB.add_scatter(name=titles[i],x=B_final[i*samples_per_class:i*samples_per_class+samples_per_class,0],y=B_final[i*samples_per_class:i*samples_per_class+samples_per_class,1],mode="markers",marker={'size': 15,'color': colors[i]})#,legendgrouptitle={'text':titles[i]})
+    
+
+    figA.write_image("figures/original_dataset/figA.png")
+    figB.write_image("figures/original_dataset/figB.png")
 
 def plot_p():
     
@@ -109,5 +158,6 @@ def plot_p():
 
 
 if __name__ == "__main__":
-    plot_loss()
-    plot_p()
+    plot_dataset()
+    #plot_loss()
+    #plot_p()

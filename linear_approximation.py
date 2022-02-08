fd7e1009d896177bfa18375f977390d33ad75695
sperli14 <sperli14@msu.edu>
2022-02-08 18:37:53 -0500

    Polynomial approximation graph generated

--- linear_approximation.py
+++ linear_approximation.py
@@ -1,13 +1,22 @@
 
 import numpy as np
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline, Pipeline
+#from sklearn.preprocessing import PolynomialFeatures
+#from sklearn.pipeline import make_pipeline, Pipeline
 
 import matplotlib.pyplot as plt
 
 from scipy.optimize import curve_fit
+
+import torch
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas
+import ast
+import os
+from plotly.subplots import make_subplots
+import imageio
 
 
 #def poly(x, a1, b1, c1, d1, e1, a2, b2, c2, d2, e2):
@@ -38,7 +47,13 @@ def poly(x, a1, b1, c1, a2, b2, c2):
     """
 
 def linear_approximation():
-    x = [1 * i for i in range(500,4000)]
+    #np.random.seed(1000)
+    
+    if not os.path.exists("figures"):
+        os.mkdir("figures")
+    if not os.path.exists("figures/polynomial_approximations"):
+        os.mkdir("figures/polynomial_approximations")
+    x = [1.0 * i for i in range(500,4000)]
     #x = [1 * i for i in range(1000,5000)]
     y = [1/(i**0.5) for i in x]
     x = np.array(x)#.reshape(-1,1)
@@ -49,10 +64,45 @@ def linear_approximation():
     
     print(popt)
     
+    #new_y = [poly(i, *popt) for i in x]
+    #x = [0.1 * i for i in range(5000,40000)]
+    #new_y = []
+    #for i in x:
+        #result = poly(i, *popt)
+        #print(i, result)
+        #new_y.append(result)
+    #print(x)
     plt.plot(x, poly(x, *popt), 'r-', label="label")
+    #plt.plot(x, new_y, 'r-', label="label")
     plt.plot(x, y, 'r-', label="label")
     
-    plt.show()
+    #plt.show()
+    
+    
+    
+    fig = go.Figure(layout = go.Layout(title = go.layout.Title(text="Polynomial Approximation")))
+    fig.add_trace(
+        go.Scatter(
+            mode='lines',
+            x=x,
+            y=poly(x, *popt),
+            marker={'color':'red'},
+            showlegend=False
+        )   
+    )
+    fig.add_trace(
+        go.Scatter(
+            mode='lines',
+            x=x,
+            y=y,
+            marker={'color':'black'},
+            showlegend=False
+        )  
+    )
+    
+    fig_file_name = "figures/polynomial_approximations/approx1.png"
+    fig.write_image(fig_file_name)
+    #print(poly(4000, *popt), y[3499])
     
     """
     reg = LinearRegression().fit(x, y)

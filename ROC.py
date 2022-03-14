a6916ff0bce5cf42c1fa6864764c5612a65e249f
sperli14 <sperli14@msu.edu>
2022-03-14 15:46:04 -0400

    Hyperparameter selection implemented

--- ROC.py
+++ ROC.py
@@ -81,6 +81,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas
 import ast
+from operator import itemgetter
+
 
 from sklearn import metrics
 
@@ -173,6 +175,23 @@ def ROC(filename, tag, title):
     """
     print(fps)
     print(tps)
+    
+    auc = 0
+    
+    auc_dict = {}
+    for i in range(len(tps)):
+        auc_dict[fps[i]] = tps[i]
+    auc_list = list(auc_dict.items())
+    auc_list.sort(key=itemgetter(0))
+    
+    for i in range(1,len(auc_list)):
+        interval = abs(auc_list[i][0]-auc_list[i-1][0])
+        auc += interval*auc_list[i-1][1]
+        auc += 0.5 * interval * (auc_list[i][1]-auc_list[i-1][1])
+        
+    #print()
+    #print(auc_list)
+    print("AUC:",auc)
 
 def ROC2(filename, gamma, lamb, title):
     enc_results_file = open(filename,'r')
@@ -231,9 +250,195 @@ def ROC2(filename, gamma, lamb, title):
     fig_file_name = "figures/ROC_projected_gamma=" + str(gamma) + "_lambda=" + str(lamb) + ".png"
     fig.write_image(fig_file_name)
     
+    auc = 0
+    
+    auc_dict = {}
+    for i in range(len(tps)):
+        auc_dict[fps[i]] = tps[i]
+    auc_list = list(auc_dict.items())
+    auc_list.sort(key=itemgetter(0))
+    
+    for i in range(1,len(auc_list)):
+        interval = abs(auc_list[i][0]-auc_list[i-1][0])
+        auc += interval*auc_list[i-1][1]
+        auc += 0.5 * interval * (auc_list[i][1]-auc_list[i-1][1])
+        
+    #print()
+    #print(auc_list)
+    print("AUC:",auc)
+    
     print(fps)
     print(tps)
+    #print()
+    
+def ROC_P_Matrix(filename, gamma, lamb, title):
+    p_file = open(filename,'r')
+    p = []
+    #L = []
+    for line in p_file:
+        #result, l = line.strip().split(";")
+        #print(line)
+        result = torch.tensor(ast.literal_eval(line.strip()))
+        #l = int(l)
+        p.append(result)
+        #L.append(l)
+        p_final = torch.Tensor(len(p),p[0].shape[0])
+    torch.cat(p, out=p_final,dim=0)
+    
+    thresholds = [0.05 * i for i in range(41)]
+    thresholds = [0.01 * i for i in range(201)]
+    
+    #num_class = len(set(L))
+    #samples_per_class = L.count(L[0])
+    
+    
+    a_file = open("data/features_A_values_test.txt",'r')
+    A = []
+    L = []
+    for line in a_file:
+        line, l = line.strip().split(";")
+        L.append(float(l))
+        a = torch.tensor(ast.literal_eval(line.strip())).unsqueeze(dim=0)
+        A.append(a)
+    A_final = torch.Tensor(len(A),A[0].shape[0])
+    torch.cat(A, out=A_final,dim=0)
+    
+    b_file = open("data/features_B_values_test.txt",'r')
+    B = []
+    for line in b_file:
+        line, l = line.strip().split(";")
+        b = torch.tensor(ast.literal_eval(line.strip())).unsqueeze(dim=0)
+        B.append(b)
+    B_final = torch.Tensor(len(B),B[0].shape[0])
+    torch.cat(B, out=B_final,dim=0)
+        
+    #print("here")
+    #l_file = open("data/features_labels_X_prime_test_lambda=0.99_margin=0.5_gamma=128.txt",'r')
+    #L = []
+    #for line in l_file:
+        #L.append(float(line.strip().split(";")[1]))
+    
+    
+    #Create feature fusion dataset
+    X = torch.cat((A_final,B_final),dim=1)
+    
+    X_prime = torch.mm(p_final,X.T)
+    for i in range(X_prime.shape[0]):
+        X_prime[i,:]=torch.div(X_prime[i,:], torch.linalg.norm(X_prime[i,:]))
+    
+    
+    #count = num_class * samples_per_class
+    #count = 90
+    #count = 10
+    count = len(L)
+    fps = []
+    tps = []
+    for threshold in thresholds:
+        fp = 0
+        tp = 0
+        pc = 0
+        nc = 0
+        for i in range(count):
+            for j in range(i,count):
+                guess = False
+                if Cosine_Distance(X_prime[i],X_prime[j]) <= threshold:
+                    guess = True
+                if L[i]==L[j]:
+                    pc += 1
+                    if guess:
+                        tp += 1
+                else:
+                    nc += 1
+                    if guess:
+                        fp += 1
+        fp = fp/nc
+        tp = tp/pc
+        fps.append(fp)
+        tps.append(tp)
+        
+    data_dict = {"False Positive Rate":fps,"True Positive Rate":tps}
+    df = pandas.DataFrame(data_dict)
+    fig = px.line(df,x="False Positive Rate",y="True Positive Rate",
+                  title=title)
+    fig.update_yaxes(range=[0,1])
+    
+    fig_file_name = "figures/ROC_projected_gamma=" + str(gamma) + "_lambda=" + str(lamb) + ".png"
+    fig.write_image(fig_file_name)
+    
+    auc = 0
+    
+    auc_dict = {}
+    for i in range(len(tps)):
+        auc_dict[fps[i]] = tps[i]
+    auc_list = list(auc_dict.items())
+    auc_list.sort(key=itemgetter(0))
+    
+    for i in range(1,len(auc_list)):
+        interval = abs(auc_list[i][0]-auc_list[i-1][0])
+        auc += interval*auc_list[i-1][1]
+        auc += 0.5 * interval * (auc_list[i][1]-auc_list[i-1][1])
+        
+    #print()
+    #print(auc_list)
+    print("AUC:",auc)
+    
+    print(fps)
+    print(tps)
+    #print()
 
+def ROC_AUC(data, L):
+    thresholds = [0.05 * i for i in range(41)]
+    thresholds = [0.01 * i for i in range(201)]
+    
+    count = L.size(0)
+    fps = []
+    tps = []
+    for threshold in thresholds:
+        fp = 0
+        tp = 0
+        pc = 0
+        nc = 0
+        for i in range(count):
+            for j in range(i,count):
+                guess = False
+                if Cosine_Distance(data[i],data[j]) <= threshold:
+                    guess = True
+                if L[i]==L[j]:
+                    pc += 1
+                    if guess:
+                        tp += 1
+                else:
+                    nc += 1
+                    if guess:
+                        fp += 1
+        fp = fp/nc
+        tp = tp/pc
+        fps.append(fp)
+        tps.append(tp)
+        
+    """
+    data_dict = {"False Positive Rate":fps,"True Positive Rate":tps}
+    df = pandas.DataFrame(data_dict)
+    fig = px.line(df,x="False Positive Rate",y="True Positive Rate",
+                  title=title)
+    fig.update_yaxes(range=[0,1])
+    
+    fig_file_name = "figures/ROC_projected_gamma=" + str(gamma) + "_lambda=" + str(lamb) + ".png"
+    fig.write_image(fig_file_name)
+    """
+    auc = 0
+    
+    auc_dict = {}
+    for i in range(len(tps)):
+        auc_dict[fps[i]] = tps[i]
+    auc_list = list(auc_dict.items())
+    auc_list.sort(key=itemgetter(0))
+    
+    for i in range(1,len(auc_list)):
+        interval = abs(auc_list[i][0]-auc_list[i-1][0])
+        auc += interval*auc_list[i-1][1]
+        auc += 0.5 * interval * (auc_list[i][1]-auc_list[i-1][1])
+    return auc
 
 if __name__ == "__main__":
     ROC("data/features_A_values_test.txt", "A", "ROC - MMU Iris Resnet 1024-dimensional Features")
@@ -241,8 +446,11 @@ if __name__ == "__main__":
     ROC("data/features_X_values_test.txt", "X", "ROC - MMU Iris 1536-dimensional Concatenated Features")
     #ROC2("data/features_labels_best_P_value_transpose_lambda=0.5_margin=0.5_gamma=2.txt", 2, "ROC Projected Dataset γ=2 (Normalized)")
     #ROC2("data/features_labels_best_P_value_transpose_lambda=0.5_margin=0.5_gamma=256.txt", 256, 0.5, "ROC Projected Dataset γ=256 (Normalized) λ=0.5")
-    ROC2("data/features_labels_X_prime_test_lambda=0.25_margin=0.5_gamma=256.txt", 256, 0.25, "ROC Projected Test Dataset γ=256 (Normalized) λ=0.25")
+    ##ROC2("data/features_labels_X_prime_test_lambda=0.25_margin=0.5_gamma=256.txt", 256, 0.25, "ROC Projected Test Dataset γ=256 (Normalized) λ=0.25")
     #ROC2("data/features_labels_best_P_value_transpose_lambda=0.75_margin=0.5_gamma=256.txt", 256, 0.75, "ROC Projected Dataset γ=256 (Normalized) λ=0.75")
     #ROC2("data/features_labels_best_P_value_transpose_lambda=0.99_margin=0.5_gamma=256.txt", 256, 0.99, "ROC Projected Dataset γ=256 (Normalized) λ=0.99")
     #ROC2("data/features_labels_best_P_value_transpose_lambda=0.5_margin=0.5_gamma=512.txt", 512, "ROC Projected Dataset γ=512 (Normalized)")
+    #ROC_P_Matrix("data/features_best_P_value_transpose_lambda=0.1_margin=0.25_gamma=128.txt", 128, 0.1, "ROC Projected Test Dataset γ=128 (Normalized) λ=0.1, margin=0.25")
+    #ROC_P_Matrix("data/features_best_P_value_transpose_lambda=0.1_margin=0.25_gamma=256.txt", 256, 0.1, "ROC Projected Test Dataset γ=256 (Normalized) λ=0.1, margin=0.25")
+    #ROC_P_Matrix("data/features_best_P_value_transpose_lambda=0.25_margin=0.5_gamma=256.txt", 256, 0.1, "ROC Projected Test Dataset γ=256 (Normalized) λ=0.1, margin=0.25")
 
--- train.py
+++ train.py
@@ -13,7 +13,9 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 from model import Linear_Feature_Fusion
 from data_generation import data_gen
 
-def train(gamma,iters):
+from ROC import ROC_AUC
+
+def train(gamma,iters,spec_margin=None,spec_lamb=None):
     random.seed(0)
     torch.manual_seed(0)
     #A,B,L = data_gen(10)
@@ -117,126 +119,129 @@ def train(gamma,iters):
     
     #Hyperparameters
     #gamma = 512
-    lambs = [0.25,0.5,0.75,0.99]
-    lambs = [0.25]
-    margins = [0.5]
+    lambs = [0.1,0.25,0.5,0.75,0.99]
+    #lambs = [0.25]
+    margins = [0.0,0.25,0.5]
     #lambs = [0.5]
     margin = 0.5
     iterations = iters
     #iterations = 100
     #break_point = 0.005
-    rates = {2:1000, 256:10000, 512:10000}
+    rates = {2:1000, 128: 10000, 256:10000, 512:10000}
     
-    
+    if spec_margin:
+        margins = [spec_margin]
+    if spec_lamb:
+        lambs = [spec_lamb]
     
     
     #lamb = 0.2
-    for lamb in lambs:
-        rate = rates[gamma]
-        print("Lambda value of", lamb)
-        
-        M = []
-        V = []
-        for i in range(X_train.shape[0]):
-            for j in range(i+1,X_train.shape[0]):
-                if L_train[i] == L_train[j]:
-                    M.append((i,j))
-                    for k in range(X_train.shape[0]):
-                        if L_train[k] != L_train[i]:
-                            V.append((i,j,k))
-        print("Same class",len(M))
-        print("Trios:",len(V))
-        
-        
-        
-        
-        
-        model = Linear_Feature_Fusion(X_train,M,V,gamma,margin,lamb)
-        
-
-    
-        best_loss = model.loss()
-        best_P = model.P
-        print("Initial loss:",best_loss)
-        P_history = []
-        P_history_matrices = []
-        losses = []
-        optim = torch.optim.SGD(model.parameters(), lr=rate)#, momentum=0.9)
-        for i in range(iterations):
-            loss = model.loss()
-            if loss < best_loss:
-                best_loss = loss
-                best_P = model.P
-            losses.append(loss)
-            P_history.append(str(model.P.tolist()))
-            P_history_matrices.append(model.P)
-            loss.backward()
-            optim.step()
-            print(model.loss())
-            if i%10 == 0:
-                print("Iteration",str(i) + "/" + str(iterations))
-            rate *= 0.95
-        print("final loss:",model.loss())
-        
-
-        #P values file gets too large for github
-        #p_file_name = "data/features_P_values_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma) + ".txt"
-        #outfile_p = open(p_file_name,'w')
-        #for P_value in P_history:
-            #outfile_p.write(P_value)
-            #outfile_p.write("\n")
-        #outfile_p.close()
-        
-        p_best_file_name = "data/features_best_P_value_transpose_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma)  + ".txt"
-        outfile_p_t = open(p_best_file_name,'w')
-        P_final_t = best_P.T
-        P_final_t = str(P_final_t.tolist())
-        outfile_p_t.write(P_final_t)
-        outfile_p_t.close()
-        
-        loss_file_name = "data/features_loss_values_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma)  + ".txt"
-        outfile_loss = open(loss_file_name,'w')
-        for loss_value in losses:
-            outfile_loss.write(str(loss_value.tolist()))
-            outfile_loss.write("\n")
-        outfile_loss.close()
-        print()
-        
-        #X_prime = torch.mm(X_val,P_history_matrices[-1])
-        X_prime = torch.mm(X_val,best_P)
-        for i in range(X_prime.shape[0]):
-            X_prime[i,:]=torch.div(X_prime[i,:], torch.linalg.norm(X_prime[i,:]))
-        print("new features:", X_prime)
-        
-        X_prime_filename = "data/features_labels_X_prime_val_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma) + ".txt"
-        outfile_x = open(X_prime_filename,'w')
-        x_list = X_prime.tolist()
-        for i in range(len(x_list)):
-            x_list[i] = str(x_list[i])+";"+str(L_val.tolist()[i])
-            outfile_x.write(x_list[i])
-            outfile_x.write("\n")
-        #x_str = str(x_list)
-        #outfile_x.write(x_str)
-        outfile_x.close()
-        
-        #X_prime = torch.mm(X_val,P_history_matrices[-1])
-        X_prime = torch.mm(X_test,best_P)
-        for i in range(X_prime.shape[0]):
-            X_prime[i,:]=torch.div(X_prime[i,:], torch.linalg.norm(X_prime[i,:]))
-        print("new features:", X_prime)
-        
-        X_prime_filename = "data/features_labels_X_prime_test_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma) + ".txt"
-        outfile_x = open(X_prime_filename,'w')
-        x_list = X_prime.tolist()
-        for i in range(len(x_list)):
-            x_list[i] = str(x_list[i])+";"+str(L_test.tolist()[i])
-            outfile_x.write(x_list[i])
-            outfile_x.write("\n")
-        #x_str = str(x_list)
-        #outfile_x.write(x_str)
-        outfile_x.close()
-
+    aucs = []
+    for margin in margins:
+        for lamb in lambs:
+            rate = rates[gamma]
+            print("Lambda value of", lamb, "Margin value of", margin)
+            
+            M = []
+            V = []
+            for i in range(X_train.shape[0]):
+                for j in range(i+1,X_train.shape[0]):
+                    if L_train[i] == L_train[j]:
+                        M.append((i,j))
+                        for k in range(X_train.shape[0]):
+                            if L_train[k] != L_train[i]:
+                                V.append((i,j,k))
+            print("Same class",len(M))
+            print("Trios:",len(V))
+            
+            model = Linear_Feature_Fusion(X_train,M,V,gamma,margin,lamb)
+            
+            best_loss = model.loss()
+            best_P = model.P
+            print("Initial loss:",best_loss)
+            P_history = []
+            P_history_matrices = []
+            losses = []
+            optim = torch.optim.SGD(model.parameters(), lr=rate)#, momentum=0.9)
+            for i in range(iterations):
+                loss = model.loss()
+                if loss < best_loss:
+                    best_loss = loss
+                    best_P = model.P
+                losses.append(loss)
+                P_history.append(str(model.P.tolist()))
+                P_history_matrices.append(model.P)
+                loss.backward()
+                optim.step()
+                print(model.loss())
+                if i%10 == 0:
+                    print("Iteration",str(i) + "/" + str(iterations))
+                rate *= 0.95
+            print("final loss:",model.loss())
+            
+            X_prime = torch.mm(X_val,best_P)
+            for i in range(X_prime.shape[0]):
+                X_prime[i,:]=torch.div(X_prime[i,:], torch.linalg.norm(X_prime[i,:]))
+            auc = ROC_AUC(X_prime, L_val)
+            aucs.append((margin,lamb,auc))
+            print("AUC of lambda="+str(lamb)+", margin="+str(margin)+":", auc)
+            #P values file gets too large for github
+            #p_file_name = "data/features_P_values_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma) + ".txt"
+            #outfile_p = open(p_file_name,'w')
+            #for P_value in P_history:
+                #outfile_p.write(P_value)
+                #outfile_p.write("\n")
+            #outfile_p.close()
+            
+            p_best_file_name = "data/features_best_P_value_transpose_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma)  + ".txt"
+            outfile_p_t = open(p_best_file_name,'w')
+            P_final_t = best_P.T
+            P_final_t = str(P_final_t.tolist())
+            outfile_p_t.write(P_final_t)
+            outfile_p_t.close()
+            
+            loss_file_name = "data/features_loss_values_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma)  + ".txt"
+            outfile_loss = open(loss_file_name,'w')
+            for loss_value in losses:
+                outfile_loss.write(str(loss_value.tolist()))
+                outfile_loss.write("\n")
+            outfile_loss.close()
+            print()
+            
+            #X_prime = torch.mm(X_val,P_history_matrices[-1])
+            #X_prime = torch.mm(X_val,best_P)
+            #for i in range(X_prime.shape[0]):
+                #X_prime[i,:]=torch.div(X_prime[i,:], torch.linalg.norm(X_prime[i,:]))
+            print("new features:", X_prime)
+            
+            X_prime_filename = "data/features_labels_X_prime_val_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma) + ".txt"
+            outfile_x = open(X_prime_filename,'w')
+            x_list = X_prime.tolist()
+            for i in range(len(x_list)):
+                x_list[i] = str(x_list[i])+";"+str(L_val.tolist()[i])
+                outfile_x.write(x_list[i])
+                outfile_x.write("\n")
+            #x_str = str(x_list)
+            #outfile_x.write(x_str)
+            outfile_x.close()
+            
+            #X_prime = torch.mm(X_val,P_history_matrices[-1])
+            X_prime = torch.mm(X_test,best_P)
+            for i in range(X_prime.shape[0]):
+                X_prime[i,:]=torch.div(X_prime[i,:], torch.linalg.norm(X_prime[i,:]))
+            print("new features:", X_prime)
+            
+            X_prime_filename = "data/features_labels_X_prime_test_lambda=" + str(lamb) + "_margin=" + str(margin) + "_gamma=" + str(gamma) + ".txt"
+            outfile_x = open(X_prime_filename,'w')
+            x_list = X_prime.tolist()
+            for i in range(len(x_list)):
+                x_list[i] = str(x_list[i])+";"+str(L_test.tolist()[i])
+                outfile_x.write(x_list[i])
+                outfile_x.write("\n")
+            outfile_x.close()
+    print("(margin, lambda, Validation AUC)")
+    print(aucs)
 if __name__ == "__main__":
-    train(256,200)
+    train(256,200)#for gamma=128, use margin=0.25,lamb=0.1)
 
 
